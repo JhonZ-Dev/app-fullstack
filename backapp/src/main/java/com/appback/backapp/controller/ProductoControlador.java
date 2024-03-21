bb128092c39c0571ba82135c9f1ef6ddd4d64b81
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-03-21 17:22:05 -0500

    Update ProductoControlador.java

--- backapp/src/main/java/com/appback/backapp/controller/ProductoControlador.java
+++ backapp/src/main/java/com/appback/backapp/controller/ProductoControlador.java
@@ -1,4 +1,33 @@
 package com.appback.backapp.controller;
 
+import com.appback.backapp.model.Producto;
+import com.appback.backapp.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/producto")
 public class ProductoControlador {
+
+    @Autowired
+    private ProductoService productoService;
+
+    //guardar
+    @PostMapping("/guardar")
+    public Producto insertar(Producto producto){
+        return productoService.guardar(producto);
+    }
+
+    @PutMapping("/editar")
+    public Producto edit(Producto producto, Long id_producto){
+        return productoService.editar(producto,id_producto);
+    }
+    @GetMapping("listar")
+    public List<Producto> listarproductos(){
+        return productoService.productoList();
+    }
+    @DeleteMapping("/eliminar")
+    public void
 }
