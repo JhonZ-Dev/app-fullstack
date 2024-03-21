92a000698b4591331e960015fe6393a23cede42e
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-03-21 17:06:49 -0500

    Update ProductoRepositorio.java

--- backapp/src/main/java/com/appback/backapp/repositorio/ProductoRepositorio.java
+++ backapp/src/main/java/com/appback/backapp/repositorio/ProductoRepositorio.java
@@ -4,5 +4,5 @@ import com.appback.backapp.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductoRepositorio extends JpaRepository<Producto, Long> {
-    
+
 }
