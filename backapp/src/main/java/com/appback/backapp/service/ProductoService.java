1cd30eba8394fec83608f6b9053cfa96d84c3299
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-03-24 23:03:46 -0500

    Update ProductoService.java

--- backapp/src/main/java/com/appback/backapp/service/ProductoService.java
+++ backapp/src/main/java/com/appback/backapp/service/ProductoService.java
@@ -16,7 +16,9 @@ public class ProductoService {
 
 
     //metodo para crear
-  
+    public Producto guardar (Producto producto){
+        return repositorio.save(producto);
+    }
 
     //listar
     public List<Producto> productoList(){
