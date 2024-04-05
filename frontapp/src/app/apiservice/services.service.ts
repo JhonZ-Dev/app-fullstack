275e3be0f702a70116a9446928464f6a1fbcbcab
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-04-05 11:57:04 -0500

    Update services.service.ts

--- frontapp/src/app/apiservice/services.service.ts
+++ frontapp/src/app/apiservice/services.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Productos } from '../producto/productos';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicesService {
 
-  url="http://localhost:8080/api"
+  url="http://localhost:8080/api/prodcuto"
 
   constructor(private http:HttpClient) { }
+
+
+ 
 
 
 }
